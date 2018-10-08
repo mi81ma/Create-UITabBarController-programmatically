2a71fb5343167d77cb3040b9363739ba452cded2
masato <masato@m-mbp.local>
2018-10-08 16:08:12 +0800

    initial commit

--- Create UITabBarController programmatically/AppDelegate.swift
+++ Create UITabBarController programmatically/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+
+// *** set Bar Color Black ***
+        UITabBar.appearance().barTintColor = UIColor.black
+        
         return true
     }
 
---
+++ Create UITabBarController programmatically/ViewControllerForTab1.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Create UITabBarController programmatically
+//
+//  Created by masato on 8/10/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+//  Swift 4.2
+
+import UIKit
+class TabOneViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.blue
+        self.title = "Tab 1"
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
